6da01725336c408b229af8954aadb8f9d5e12c95
horacio <hegu224@gmail.com>
2014-06-09 21:55:20 -0300

    agregue campos a las tablas

---
+++ protected/data/dbinmweb 2014-06-09.sql
@@ -0,0 +1,256 @@
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+CREATE SCHEMA IF NOT EXISTS `dbinmweb` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+USE `dbinmweb` ;
+
+-- -----------------------------------------------------
+-- Table `dbinmweb`.`pais`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `dbinmweb`.`pais` (
+  `idpais` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`idpais`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `dbinmweb`.`ciudad`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `dbinmweb`.`ciudad` (
+  `idciudad` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(45) NOT NULL,
+  `paisid` INT NOT NULL,
+  PRIMARY KEY (`idciudad`),
+  INDEX `fk_pais_ciudad_idx` (`paisid` ASC),
+  CONSTRAINT `fk_pais_ciudad`
+    FOREIGN KEY (`paisid`)
+    REFERENCES `dbinmweb`.`pais` (`idpais`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `dbinmweb`.`barrio`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `dbinmweb`.`barrio` (
+  `idbarrio` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(45) NOT NULL,
+  `ciudadid` INT NOT NULL,
+  PRIMARY KEY (`idbarrio`),
+  INDEX `fk_ciudadid_idx` (`ciudadid` ASC),
+  CONSTRAINT `fk_ciudadid`
+    FOREIGN KEY (`ciudadid`)
+    REFERENCES `dbinmweb`.`ciudad` (`idciudad`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `dbinmweb`.`usuario`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `dbinmweb`.`usuario` (
+  `idusuario` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(60) NOT NULL,
+  `apellido` VARCHAR(60) NOT NULL,
+  `email` VARCHAR(60) NOT NULL,
+  `password` VARCHAR(60) NOT NULL,
+  `telefono` VARCHAR(45) NOT NULL,
+  `celular` VARCHAR(45) NOT NULL,
+  `tipousuarioid` CHAR NOT NULL,
+  PRIMARY KEY (`idusuario`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `dbinmweb`.`empleado`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `dbinmweb`.`empleado` (
+  `idusuario` INT NOT NULL AUTO_INCREMENT,
+  `numero` INT NOT NULL,
+  `sueldo` MEDIUMTEXT NOT NULL,
+  `fechacreacion` DATE NOT NULL,
+  `fechamodificacion` DATE NULL,
+  `usuariocreacion` INT NOT NULL,
+  `usuariomodificacion` INT NULL,
+  PRIMARY KEY (`idusuario`),
+  CONSTRAINT `fk_empleado`
+    FOREIGN KEY (`idusuario`)
+    REFERENCES `dbinmweb`.`usuario` (`idusuario`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `dbinmweb`.`cliente`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `dbinmweb`.`cliente` (
+  `idusuario` INT NOT NULL AUTO_INCREMENT,
+  `fechacreacion` DATE NOT NULL,
+  `fechamodificacion` DATE NULL,
+  `usuariocreacion` INT NOT NULL,
+  `usuariomodificacion` INT NULL,
+  PRIMARY KEY (`idusuario`),
+  CONSTRAINT `fk_cliente`
+    FOREIGN KEY (`idusuario`)
+    REFERENCES `dbinmweb`.`usuario` (`idusuario`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `dbinmweb`.`propiedad`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `dbinmweb`.`propiedad` (
+  `idpropiedad` INT NOT NULL AUTO_INCREMENT,
+  `canthab` INT NOT NULL,
+  `cantbano` INT NOT NULL,
+  `terreno` INT NULL,
+  `construido` INT NULL,
+  `garage` TINYINT(1) NULL,
+  `jardin` TINYINT(1) NULL,
+  `fondo` TINYINT(1) NULL,
+  `precio` FLOAT NOT NULL,
+  `descripcion` VARCHAR(150) NOT NULL,
+  `ingreso` DATE NOT NULL,
+  `egreso` DATE NULL,
+  `clienteid` INT NOT NULL,
+  `empleadoid` INT NOT NULL,
+  `fechacreacion` DATE NOT NULL,
+  `fechamodificacion` DATE NULL,
+  `usuariocreacion` INT NOT NULL,
+  `usuariomodificacion` INT NULL,
+  PRIMARY KEY (`idpropiedad`),
+  INDEX `fk_empleado_idx` (`empleadoid` ASC),
+  INDEX `fk_cliente_idx` (`clienteid` ASC),
+  CONSTRAINT `fk_agente`
+    FOREIGN KEY (`empleadoid`)
+    REFERENCES `dbinmweb`.`empleado` (`idusuario`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_dueno`
+    FOREIGN KEY (`clienteid`)
+    REFERENCES `dbinmweb`.`cliente` (`idusuario`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `dbinmweb`.`ubicacion`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `dbinmweb`.`ubicacion` (
+  `idubicacion` INT NOT NULL AUTO_INCREMENT,
+  `direccion` VARCHAR(100) NOT NULL,
+  `latitudlongitud` VARCHAR(100) NULL,
+  `barrioid` INT NOT NULL,
+  `propiedadid` INT NOT NULL,
+  PRIMARY KEY (`idubicacion`),
+  INDEX `fk_barrio_idx` (`barrioid` ASC),
+  INDEX `fk_propiedad_ubicacion_idx` (`propiedadid` ASC),
+  CONSTRAINT `fk_barrio_ubicacion`
+    FOREIGN KEY (`barrioid`)
+    REFERENCES `dbinmweb`.`barrio` (`idbarrio`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_propiedad_ubicacion`
+    FOREIGN KEY (`propiedadid`)
+    REFERENCES `dbinmweb`.`propiedad` (`idpropiedad`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `dbinmweb`.`imagen`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `dbinmweb`.`imagen` (
+  `idimagen` INT NOT NULL AUTO_INCREMENT,
+  `archivo` VARCHAR(100) NOT NULL,
+  `propiedadid` INT NOT NULL,
+  `fechacreacion` DATE NOT NULL,
+  `fechamodificacion` DATE NULL,
+  `usuariocreacion` INT NOT NULL,
+  `usuariomodificacion` INT NULL,
+  PRIMARY KEY (`idimagen`),
+  INDEX `fk_propiedad_idx` (`propiedadid` ASC),
+  CONSTRAINT `fk_propiedad`
+    FOREIGN KEY (`propiedadid`)
+    REFERENCES `dbinmweb`.`propiedad` (`idpropiedad`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `dbinmweb`.`requerida`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `dbinmweb`.`requerida` (
+  `idrequerida` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(60) NOT NULL,
+  `apellido` VARCHAR(60) NOT NULL,
+  `email` VARCHAR(60) NOT NULL,
+  `descripcion` VARCHAR(150) NOT NULL,
+  `barrioid` INT NOT NULL,
+  PRIMARY KEY (`idrequerida`),
+  INDEX `fk_barrio_idx` (`barrioid` ASC),
+  CONSTRAINT `fk_barrio_requerida`
+    FOREIGN KEY (`barrioid`)
+    REFERENCES `dbinmweb`.`barrio` (`idbarrio`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `dbinmweb`.`destacado`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `dbinmweb`.`destacado` (
+  `iddestacado` INT NOT NULL AUTO_INCREMENT,
+  `fechainicio` DATETIME NOT NULL,
+  `fechafin` DATETIME NOT NULL,
+  `idpropiedad` INT NOT NULL,
+  `fechacreacion` DATE NOT NULL,
+  `fechamodificacion` DATE NULL,
+  `usuariocreacion` INT NOT NULL,
+  `usuariomodificacion` INT NULL,
+  PRIMARY KEY (`iddestacado`),
+  INDEX `id_propiedad_destacado_idx` (`idpropiedad` ASC),
+  CONSTRAINT `id_propiedad_destacado`
+    FOREIGN KEY (`idpropiedad`)
+    REFERENCES `dbinmweb`.`propiedad` (`idpropiedad`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `dbinmweb`.`visitas`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `dbinmweb`.`visitas` (
+  `idvisitas` INT NOT NULL AUTO_INCREMENT,
+  `idpropiedad` INT NOT NULL,
+  `fecha` DATE NOT NULL,
+  `hora` TIME NOT NULL,
+  `fechacreacion` DATE NOT NULL,
+  `fechamodificacion` DATE NULL,
+  `usuariocreacion` INT NOT NULL,
+  `usuariomodificacion` INT NULL,
+  PRIMARY KEY (`idvisitas`),
+  INDEX `id_propiedad_visita_idx` (`idpropiedad` ASC),
+  CONSTRAINT `id_propiedad_visita`
+    FOREIGN KEY (`idpropiedad`)
+    REFERENCES `dbinmweb`.`propiedad` (`idpropiedad`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
