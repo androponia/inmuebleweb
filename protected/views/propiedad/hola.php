52293fa12c3fda25185b0ba8c1af00aed9e99390
lautaroa14 <lautaroa14@gmail.com>
2014-07-15 00:46:26 -0300

    El estilo del buscador

--- protected/views/propiedad/hola.php
+++ protected/views/propiedad/hola.php
@@ -7,21 +7,32 @@ if(empty($propiedades))
 }
 else
 {
-
+	echo '<div class="container">';
+	echo '<div clss="row">';
 	$i = 0;
 	foreach ($propiedades as $key){
-
-		echo 'ID PROPIEDAD: '.$key[0]['idpropiedad'].'<br>';
-		echo 'DIRECCION: '.$ubicaciones[$i]['direccion'].'<br>';
-		echo 'PRECIO: '.$key[0]['precio'].'<br>';
-		echo 'HABITACIONES: '.$key[0]['canthab'].'<br>';
-		echo 'BAÑOS: '.$key[0]['cantbano'].'<br>';
-		echo 'GARAGE: '.$key[0]['garage'].'<br>';
-		echo 'JARDIN: '.$key[0]['jardin'].'<br>';
-		echo 'METROS TERRENO: '.$key[0]['terreno'].'<br>';
-		echo 'METROS CONSTRUIDOS: '.$key[0]['construido'].'<br>';
-		echo 'DESCRIPCION: '.$key[0]['descripcion'].'<br><br><br>';
+		
+		echo '<a class="enlacePropiedad" href="#">';
+		echo '<div class="contPropiedad">';
+		echo '<div class="contPropInfo" style="float:left;margin-left:15px;margin-top: 15px;width:250px; height: 200px; ">';
+		//echo 'ID PROPIEDAD: '.$key[0]['idpropiedad'].'<br>';
+		echo '<strong>DIRECCION: </strong>'.$ubicaciones[$i]['direccion'].'<br>';
+		echo '<strong>PRECIO: </strong>'.$key[0]['precio'].'<br>';
+		echo '<strong>HABITACIONES: </strong>'.$key[0]['canthab'].'<br>';
+		echo '<strong>BAÑOS: </strong>'.$key[0]['cantbano'].'<br>';
+		echo '<strong>GARAGE: </strong>'.$key[0]['garage'].'<br>';
+		echo '<strong>JARDIN: </strong>'.$key[0]['jardin'].'<br>';
+		echo '<strong>METROS TERRENO: </strong>'.$key[0]['terreno'].'<br>';
+		echo '<strong>METROS CONSTRUIDOS: </strong>'.$key[0]['construido'].'<br>';
+		echo '<strong>DESCRIPCION: </strong>'.$key[0]['descripcion'].'<br><br><br>';
+		echo '</div>';
+		echo '</div>';
+		echo '</a>';
 		$i= $i+1;
 	}
+		
+		echo '</div>';//div row
+		echo '</div>';//div container
+
 }
 ?>
